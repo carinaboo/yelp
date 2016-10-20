6ad39b90521e6eeec0eff4903ccf3d517ef12098
Carina Boo <cboo@cboo-macbookpro.roam.corp.google.com>
2016-10-20 15:58:32 -0500

    Add FiltersViewController and SwitchCell. Still need to add Yelp categories.

---
+++ Yelp/FiltersViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FiltersViewController.swift
+//  Yelp
+//
+//  Created by Carina Boo on 10/20/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class FiltersViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onCancelButton(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onSearchButton(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
